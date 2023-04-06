574dc5a9e7924e9fafa520ec03265e85aafcd3f6
tresjuess <113621221+tresjuess@users.noreply.github.com>
2023-04-06 13:54:19 +0300

    Add files via upload
    Тест вставки своего спрайта

---
+++ sample.py
@@ -0,0 +1,72 @@
+import pygame
+import random
+import os
+
+WIDTH = 800
+HEIGHT = 650
+FPS = 30
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+game_folder = os.path.dirname('simcli')
+img_folder = os.path.join(game_folder, 'img')
+player_img = pygame.image.load(os.path.join(img_folder, 'mario.png'))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+player_img.set_colorkey(WHITE)
+scale = pygame.transform.scale(
+    player_img, (player_img.get_width() / 5,
+               player_img.get_height() / 5))
+scale_rect = scale.get_rect(
+    center=(200, 150))
+screen.blit(scale, scale_rect)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+
+    # Рендеринг
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
